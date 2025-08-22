25e92fd67341e2294f000e1866a0d40c9079481e
ajmal-uk <ajmaluk.me@gmail.com>
2025-08-22 19:10:59 +0530

    first commit

---
+++ DS/queue.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define Max 100
+
+
+int frond, rear , max , i , item , option;
+
+void enQueue();
+void deQueue();
+void traversal();
+void search();
---
+++ DS/stack.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define Max 100
+int i,top, maxS, item;
+int stack[Max];
+void push();
+void pop();
+void traversal();
+void search();
+
+int main(){
+    top = -1;
+    printf("\nEnter the Maximum Size of Stack : ");
+    scanf("%d",&maxS);
+    int option;
+    while(1){
+        printf("\n\n1.Push\n2.Pop\n3.Traversal\n4.Search\n5.Exit\n\nEnter the Option : ");
+        scanf("%d",&option);
+        switch (option)
+        {
+            case 1 : push(); break;
+            case 2 : pop(); break;
+            case 3 : traversal(); break;
+            case 4 : search(); break;
+            default:
+                break;
+        }
+    }
+}
+
+
+void push(){
+    if(top == maxS-1){
+        printf("\nOverflow");
+
+    }
+    else{
+        printf("\nEnter the item to Insert in Stack  : ");
+        scanf("%d",&item);
+        stack[top++] = item;
+        printf("\n%d inserted Sucess fully!!",item);
+    }
+}
+
+void pop(){
+    if(top == -1){
+        printf("\nUnderflow");
+    }
+    else{
+        printf("\n%d Removed!!" , stack[top]);
+        top--;
+    }
+}
+
+void traversal(){
+    printf("\n");
+    for(i = top; i>=0; i--){
+        printf("| %d |\n", stack[i]);
+        printf(" ___ \n");
+    }
+}
+
+void search(){
+    printf("\nEnter the Item to to search  : ");
+    scanf("%d",&item);
+    for(i = top; i>=0; i--){
+        if(stack[i] == item){
+            printf("\nItem ' %d ' Found Success Fully. At the %d",item,i);
+        }
+    }
+}
