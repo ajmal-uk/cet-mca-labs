d045ac083ce77b7fb1663e46b27b8e60653b63fe
ajmal-uk <ajmaluk.me@gmail.com>
2025-11-05 12:19:19 +0530

    ......................Graphics........................... New Change , I it is Unwanted

--- SEM1/PYTHON/cycle 5/3.py
+++ SEM1/PYTHON/cycle 5/3.py
@@ -2,10 +2,9 @@ print("<=== Read Specific Columns of given CSV file and print their content ===>
 
 import csv
 
-csv_path = "SEM1\PYTHON\cycle 5\data.csv"
 columns_to_read = ["Name", "Age"] 
 
-with open(csv_path, "r") as f:
+with open("SEM1\PYTHON\cycle 5\data.csv", "r") as f:
     reader = csv.DictReader(f) 
     for row in reader:
         selected_data = [row[col] for col in columns_to_read if col in row]
