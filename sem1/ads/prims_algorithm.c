ff627696eea99a346d92ee78693c4fee691751ef
MUHAMMED AJMAL U K <ajmaluk.me@gmail.com>
2026-01-12 11:49:10 +0530

    Refactor Prim's algorithm implementation

--- sem1/ads/prims_algorithm.c
+++ sem1/ads/prims_algorithm.c
@@ -1,44 +1,46 @@
-#include <stdio.h>
-
+#include<stdio.h>
 #define MAX 100
 #define INF 9999
 
-int main() {
-    int visited[MAX] = {0};
-    int edge_count = 0, total_cost = 0, a=-1, b=-1 , min,n=4;
-    visited[0]=1;
+int visited[MAX] = {0},edge_count=0,total_cost=0,min,u,v;
+
+int main(){
+    int n = 4;
     int adj[MAX][MAX] = {
         {0, 10, 20, 0},
         {10, 0, 5, 0},
         {20, 5, 0, 10},
         {0, 0, 10, 0}
     };
-    while(edge_count < n-1){
+
+    visited[2] = 1;
+
+    while(edge_count<n-1){
         min = INF;
-        a = -1;
-        b=-1;
+        u = -1;
+        v = -1;
         for(int i=0;i<n;i++){
             if(visited[i]){
                 for(int j=0;j<n;j++){
-                    if(!visited[j] && adj[i][j]<min && adj[i][j]!=0){
+                    if(!visited[j] && adj[i][j]!=0 && adj[i][j] < min){
                         min = adj[i][j];
-                        a=i;
-                        b=j;
+                        u = i;
+                        v = j;
                     }
                 }
             }
         }
-        
-        if (min == INF || a == -1 || b == -1) {
-            printf("Graph is disconnected — no more reachable edges.\n");
-            break;
+
+        if(v == -1){
+            printf("\ngraph is disconnected");
+            return 0;
         }
-        
-        printf("%d - %d : %d\n", a, b, min);
-        total_cost += min;
-        visited[b] = 1;
+
+        printf("%d --- %d. :: %d \n",u,v,min);
+        visited[v] = 1;
+        total_cost+=min;
         edge_count++;
     }
-    printf("\nTotal cost = %d\n", total_cost);
+    printf("total cost = %d",total_cost);
     return 0;
 }
